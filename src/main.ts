15c04761360defbfaacc63421225bf1b5f9f4a11
Tomilola-ng <tee.o2809@gmail.com>
2024-02-17 22:34:03 +0100

    SWAGGER_CONFIG

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix("api");
+
+  const config = new DocumentBuilder()
+    .setTitle("Median")
+    .setDescription("The Median API description")
+    .setVersion("0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
